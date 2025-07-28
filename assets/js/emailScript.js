6922f47684e2441ac53a3f07fc667f628c076dbd
crestharpan <arpanshrestha225@gmail.com>
2025-07-28 12:09:28 +0545

    fixing the email

--- assets/js/emailScript.js
+++ assets/js/emailScript.js
@@ -2,7 +2,8 @@ function sendMail() {
   let params = {
     name: document.getElementById("name").value,
     email: document.getElementById("mail").value,
-    text: document.getElementById("text").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("text").value,
   };
   emailjs
     .send("service_gvhxi51", "template_ypfvn3m", params)
